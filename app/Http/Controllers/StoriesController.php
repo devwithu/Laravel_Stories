2561871621485ba8b7b6a722f16d9e5be25fdc67
devwithu <devwithu@naver.com>
2021-07-12 07:46:28 +0900

    first commit

---
+++ app/Http/Controllers/StoriesController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Story;
+
+class StoriesController extends Controller
+{
+    //
+    public function index() {
+        $stories = Story::where('user_id', auth()->user()->id)
+            ->orderBy('id', 'DESC')
+            ->paginate();
+
+        return view('stories.index', ['stories' => $stories ]);
+    }
+}
